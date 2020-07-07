1ad648ab59a9397e1418eaba62efd05ad0ff2acf
Yunyun <celeph4is@gmail.com>
2020-07-06 19:41:07 -0700

    add body-parser middle-ware to parse data from post request, add cors middleware to allow requests from different origins

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const { generateInsult } = require('./helpers')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.post('/', (req, res) => {
