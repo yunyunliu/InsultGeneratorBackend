a3035b2692a16ea6cf461c8e26deceb5e65f9d93
Yunyun <celeph4is@gmail.com>
2020-07-20 18:00:21 -0700

    save progress

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static('static'))
 
 app.post('/insult', (req, res) => {
   const name = req.body.name
